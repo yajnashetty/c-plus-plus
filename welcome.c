b1704374ac723019fa53ab204bd03cab4327f567
yajnashetty <134050036+yajnashetty@users.noreply.github.com>
2025-02-25 21:17:41 +0530

    Create welcome.c
    day 1

---
+++ welcome.c
@@ -0,0 +1,14 @@
+//1. Welcome Text on Separate Line
+
+//Write a program in C++ to print welcome text on a separate line.
+#include <iostream>
+
+
+int main() {
+    // Write C++ code here
+    std::cout <<"hello"<<std::endl;
+    std::cout<<"--------------------"<<std::endl;
+    std::cout<<"this is c++ daily exercise git repo";
+
+    return 0;
+}
